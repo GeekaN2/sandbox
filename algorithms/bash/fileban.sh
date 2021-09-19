533ed13e0f9a922a3870ce368b0be7e3e900e3ca
GeekaN <geekan@bk.ru>
2021-09-19 23:44:46 +0300

    File ban

--- algorithms/bash/fileban.sh
+++ algorithms/bash/fileban.sh
@@ -1,15 +1,62 @@
+#!/bin/bash
 # loop through all files in current directory
-IFS=$'\n'
-cat ./templates.csv | while read template; do 
-    echo "$template";
 
-    IFS=$'\n'; set -f
-    findFiles="find  ./ -name " + "$template"
+hashedPassword="9e5763031b54ca938ba75dcf32e82d6976d74204ceb3ef31c54a6213624c07f0"
 
-    for f in $($findFiles); do 
-        echo "$f";
-    done
+# Script help
+if [[ "$1" =~ ^-h|--help$ ]] ; then
+    echo "Usage: `basename $0` [-h] [-p|--password=<string>]
+  -p | --password : Password to allow reading files
+"
+    exit 0
+fi
 
-    unset IFS; set +f
+# Script arguments
+for i in "$@"
+do
+case $i in
+    -p=*|--password=*)
+    password="${i#*=}"
+    ;;
+esac
 done
 
+function banread {
+    IFS=$'\n'
+    cat ./templates.csv | while read template; do
+        echo "Ban read: $template";
+        
+        IFS=$'\n'; set -f
+        findFiles="find . -name ${template}"
+        
+        for f in $(eval "$findFiles"); do
+            chmod 700 "$f";
+        done
+        
+        unset IFS; set +f
+    done
+}
+
+function unbanread {
+    IFS=$'\n'
+    cat ./templates.csv | while read template; do
+        echo "Unban read: $template";
+        
+        IFS=$'\n'; set -f
+        findFiles="find . -name ${template}"
+        
+        for f in $(eval "$findFiles"); do
+            chmod 777 "$f";
+        done
+        
+        unset IFS; set +f
+    done
+}
+
+pass=$(echo "$password" | sha256sum)
+
+if [[ "${pass:0:64}" == "$hashedPassword" ]]; then
+    unbanread
+else
+    banread
+fi
