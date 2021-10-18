9426576c0b33743805b42e08df689bf21de4d02e
geekan <geekan@bk.ru>
2021-10-18 12:58:22 +0300

    Finished that electron lab

---
+++ electron-forge-tests/note-keeper/src/demoLogic.js
@@ -0,0 +1,92 @@
+const Store = require('./store');
+const { ipcMain, ipcRenderer } = require('electron')
+const log = require('electron-log');
+
+const DEFAULT_TIME = 100;
+
+
+const store = new Store({
+  configName: 'demoLogic',
+  defaults: {
+    users: [],
+    time: DEFAULT_TIME,
+  }
+});
+
+module.exports.demoLogic = () => {
+  ipcMain.on('save-secret', (event, args) => {
+    const { user, secret } = args;
+
+    const users = store.get('users');
+    
+    if (!users[user]) {
+      store.set('users', {
+        [user]: {
+          secrets: [secret]
+        }
+      });
+
+      event.reply('show-secrets', {
+        secrets: [secret],
+        user,
+      });
+    } else {
+      const userSecrets = users[user].secrets;
+
+      if (userSecrets.length >= 4) {
+        event.reply('ban-user', user);
+
+        if (userSecrets.length === 4) {
+          store.set('users', {
+            [user]: {
+              secrets: [...userSecrets, secret]
+            }
+          });
+
+          event.reply('show-secrets', {
+            secrets: [...userSecrets, secret],
+            user,
+          });
+
+          return
+        }
+
+        event.reply('show-secrets', {
+          secrets: userSecrets,
+          user,
+        });
+
+        return;
+      }
+
+      store.set('users', {
+        [user]: {
+          secrets: [...userSecrets, secret]
+        }
+      });
+
+      event.reply('show-secrets', {
+        secrets: [...userSecrets, secret],
+        user,
+      });
+    }
+
+    return;
+  });
+
+  ipcMain.on('time-decrease', (event, arg) => {
+    let time = store.get('time');
+    log.info(time);
+    if (time === null) {
+      store.set('time', DEFAULT_TIME);
+      time = DEFAULT_TIME;
+    } 
+    
+    if (time <= 0) {
+      event.reply('on-time-exceeded');
+    } else {
+      store.set('time', time - 1);
+    }
+  })
+
+}
--- electron-forge-tests/note-keeper/src/index.js
+++ electron-forge-tests/note-keeper/src/index.js
@@ -10,8 +10,8 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -22,7 +22,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   demoLogic();
 };
