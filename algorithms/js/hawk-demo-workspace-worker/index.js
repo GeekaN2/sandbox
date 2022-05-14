e93ae0fbca9d74396433e0529d186d5f2ef19ae8
geekan <geekan@bk.ru>
2022-05-14 21:00:26 +0300

    Update demo-workspace worker

--- algorithms/js/hawk-demo-workspace-worker/index.js
+++ algorithms/js/hawk-demo-workspace-worker/index.js
@@ -2,18 +2,80 @@
 const WebSocket = require('ws');
 // worker
 
-const generateContext = () => {
-  const lyrics = ['Never gonna give you up', 'Never gonna let you down', 'Never gonna run around and desert you', 'Never gonna make you cry', 'Never gonna say goodbye', 'Never gonna tell a lie and hurt you', 'Never gonna give you up', 'Never gonna let you down', 'Never gonna run around and desert you', 'Never gonna make you cry', 'Never gonna say goodbye', 'Never gonna tell a lie and hurt you'];
+const NUMBER_OF_ERRORS = Math.floor(Math.random() * 20) + 1;
+const CONTEXT_STRINGS = [
+  "We're no strangers to love",
+  "You know the rules and so do I",
+  "A full commitment's what I'm thinking of",
+  "You wouldn't get this from any other guy",
+  "I just wanna tell you how I'm feeling",
+  "Gotta make you understand",
+  "Never gonna give you up",
+  "Never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry",
+  "Never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "We've known each other for so long",
+  "Your heart's been aching, but you're too shy to say it",
+  "Inside, we both know what's been going on",
+  "We know the game, and we're gonna play it",
+  "And if you ask me how I'm feeling",
+  "Don't tell me you're too blind to see",
+  "Never gonna give you up",
+  "Never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry",
+  "Never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "Never gonna give you up",
+  "Never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry",
+  "Never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "Ooh (Give you up)",
+  "Ooh-ooh (Give you up)",
+  "Ooh-ooh",
+  "Never gonna give, never gonna give (Give you up)",
+  "Ooh-ooh",
+  "Never gonna give, never gonna give (Give you up)",
+  "We've known each other for so long",
+  "Your heart's been aching, but you're too shy to say it",
+  "Inside, we both know what's been going on",
+  "We know the game, and we're gonna play it",
+  "I just wanna tell you how I'm feeling",
+  "Gotta make you understand",
+  "Never gonna give you up",
+  "Never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry",
+  "Never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "Never gonna give you up",
+  "Never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry",
+  "Never gonna say goodbye",
+  "Never gonna tell a lie and hurt you",
+  "Never gonna give you up",
+  "Never gonna let you down",
+  "Never gonna run around and desert you",
+  "Never gonna make you cry",
+  "Never gonna say goodbye",
+  "Never gonna tell a lie and hurt you"
+];
 
-  return lyrics[Math.floor(Math.random() * lyrics.length)];
+const generateContext = () => {
+  return CONTEXT_STRINGS[Math.floor(Math.random() * CONTEXT_STRINGS.length)];
 }
 
-const getErrorFormatted = () => ({
+const vueErrorFormatted = () => ({
   "token": "eyJpbnRlZ3JhdGlvbklkIjoiOWMzZjkxODQtZmI1ZS00MmJjLWE0Y2UtYzFlNjQwOWIzOTdhIiwic2VjcmV0IjoiYjE2MjU1MDAtY2IyYi00NjhlLWI2MzgtODViYzlmN2EwZTU0In0=",
   "catcherType": "errors/javascript",
   "payload": {
-    "title": "Test many error",
-    "type": "TypeError",
+    "title": "foo is not defined",
+    "type": "ReferenceError",
     "release": null,
     "context": {
       "rootContextSample": generateContext(),
@@ -23,19 +85,182 @@ const getErrorFormatted = () => ({
     },
     "addons": {
       "window": {
-        "innerWidth": 950,
+        "innerWidth": 874,
         "innerHeight": 799
       },
-      "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0",
-      "url": "file:///Users/demo-workspace/example/index.html"
+      "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.119 YaBrowser/22.3.0.2520 Yowser/2.5 Safari/537.36",
+      "url": "file:///Users/demo-workspace/example/index.html",
+      "vue": {
+        "lifecycle": "v-on handler",
+        "component": "<div id=\"vue-app-1\"> (root)",
+        "data": {
+          "label": "Error in Vue $root"
+        }
+      }
     },
     "backtrace": [
       {
-        "file": "file:///Users/demo-workspace/example/sample-errors.js",
-        "line": 46,
-        "column": 22,
+        "file": "file:///Users/demo-workspace/example/index.html",
+        "line": 172,
+        "column": 13,
         "sourceCode": null,
-        "function": "HTMLButtonElement.sendManyErrors"
+        "function": "Vue.buttonClicked"
+      },
+      {
+        "file": "https://unpkg.com/vue@2",
+        "line": 1872,
+        "column": 28,
+        "sourceCode": [
+          {
+            "line": 1867,
+            "content": "    vm,"
+          },
+          {
+            "line": 1868,
+            "content": "    info"
+          },
+          {
+            "line": 1869,
+            "content": "  ) {"
+          },
+          {
+            "line": 1870,
+            "content": "    var res;"
+          },
+          {
+            "line": 1871,
+            "content": "    try {"
+          },
+          {
+            "line": 1872,
+            "content": "      res = args ? handler.apply(context, args) : handler.call(context);"
+          },
+          {
+            "line": 1873,
+            "content": "      if (res && !res._isVue && isPromise(res) && !res._handled) {"
+          },
+          {
+            "line": 1874,
+            "content": "        res.catch(function (e) { return handleError(e, vm, info + \" (Promise/async)\"); });"
+          },
+          {
+            "line": 1875,
+            "content": "        // issue #9511"
+          },
+          {
+            "line": 1876,
+            "content": "        // avoid catch triggering multiple times when nested calls"
+          },
+          {
+            "line": 1877,
+            "content": "        res._handled = true;"
+          }
+        ],
+        "function": "invokeWithErrorHandling"
+      },
+      {
+        "file": "https://unpkg.com/vue@2",
+        "line": 2197,
+        "column": 16,
+        "sourceCode": [
+          {
+            "line": 2192,
+            "content": "        for (var i = 0; i < cloned.length; i++) {"
+          },
+          {
+            "line": 2193,
+            "content": "          invokeWithErrorHandling(cloned[i], null, arguments$1, vm, \"v-on handler\");"
+          },
+          {
+            "line": 2194,
+            "content": "        }"
+          },
+          {
+            "line": 2195,
+            "content": "      } else {"
+          },
+          {
+            "line": 2196,
+            "content": "        // return handler return value for single handlers"
+          },
+          {
+            "line": 2197,
+            "content": "        return invokeWithErrorHandling(fns, null, arguments, vm, \"v-on handler\")"
+          },
+          {
+            "line": 2198,
+            "content": "      }"
+          },
+          {
+            "line": 2199,
+            "content": "    }"
+          },
+          {
+            "line": 2200,
+            "content": "    invoker.fns = fns;"
+          },
+          {
+            "line": 2201,
+            "content": "    return invoker"
+          },
+          {
+            "line": 2202,
+            "content": "  }"
+          }
+        ],
+        "function": "HTMLButtonElement.invoker"
+      },
+      {
+        "file": "https://unpkg.com/vue@2",
+        "line": 7591,
+        "column": 27,
+        "sourceCode": [
+          {
+            "line": 7586,
+            "content": "          // #9448 bail if event is fired in another document in a multi-page"
+          },
+          {
+            "line": 7587,
+            "content": "          // electron/nw.js app, since event.timeStamp will be using a different"
+          },
+          {
+            "line": 7588,
+            "content": "          // starting reference"
+          },
+          {
+            "line": 7589,
+            "content": "          e.target.ownerDocument !== document"
+          },
+          {
+            "line": 7590,
+            "content": "        ) {"
+          },
+          {
+            "line": 7591,
+            "content": "          return original.apply(this, arguments)"
+          },
+          {
+            "line": 7592,
+            "content": "        }"
+          },
+          {
+            "line": 7593,
+            "content": "      };"
+          },
+          {
+            "line": 7594,
+            "content": "    }"
+          },
+          {
+            "line": 7595,
+            "content": "    target$1.addEventListener("
+          },
+          {
+            "line": 7596,
+            "content": "      name,"
+          }
+        ],
+        "function": "HTMLButtonElement.original._wrapper"
       }
     ]
   }
@@ -43,7 +268,7 @@ const getErrorFormatted = () => ({
 
 const getIntegrationId = () => {
   try {
-    const decodedIntegrationToken = JSON.parse(atob('eyJpbnRlZ3JhdGlvbklkIjoiOWMzZjkxODQtZmI1ZS00MmJjLWE0Y2UtYzFlNjQwOWIzOTdhIiwic2VjcmV0IjoiYjE2MjU1MDAtY2IyYi00NjhlLWI2MzgtODViYzlmN2EwZTU0In0='));
+    const decodedIntegrationToken = JSON.parse(atob('eyJpbnRlZ3JhdGlvbklkIjoiOGRkY2Y5ZWUtMDk0MS00NmE2LWFmNzctOTY5MDEwNTZlYzQ3Iiwic2VjcmV0IjoiNmExNDc4MGQtMDI2Mi00OTYyLTkzMDYtZmQ4ZDk1ODY5Y2RiIn0='));
     const { integrationId } = decodedIntegrationToken;
 
     if (!integrationId || integrationId === '') {
@@ -213,7 +438,7 @@ let transport = new Socket({
 const numberOfError = Math.floor(Math.random() * 20) + 1;
 
 for (let i = 0; i < numberOfError; i++) {
-  const errorFormatted = getErrorFormatted();
+  const errorFormatted = vueErrorFormatted();
 
   transport.send(errorFormatted)
     .catch((sendingError) => {
